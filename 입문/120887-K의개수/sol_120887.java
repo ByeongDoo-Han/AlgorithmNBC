35e3061ee1867b0fac0598adf8ca8594053355b3
Lee Sol <leeso4567@naver.com>
2022-12-12 12:42:22 +0900

    chore : README 파일 수정

--- 입문/120887-K의개수/sol_120887.java
+++ 입문/120887-K의개수/sol_120887.java
@@ -1,1 +1,19 @@
 // git commit -m "code: Solve programmers 120887 K의개수 (sol)"
+
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        int quotient = 0;
+        int remainder = 0;
+        for(int x = i; x <= j; x++) {
+            quotient = x;
+            while(quotient > 0) {
+                remainder = quotient % 10;
+                if(remainder == k)
+                    answer++;
+                quotient = quotient / 10;
+            }
+        }
+        return answer;
+    }
+}
