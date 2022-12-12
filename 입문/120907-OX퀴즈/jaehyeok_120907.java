9fd5febc3525a6c66923bb3b402400ba5a41d57d
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-12 17:20:47 +0900

    chore : Derectory change

--- 입문/120907-OX퀴즈/jaehyeok_120907.java
+++ 입문/120907-OX퀴즈/jaehyeok_120907.java
@@ -1,1 +1,29 @@
 // git commit -m "code: Solve programmers 120907 OX퀴즈 (jaehyeok)"
+
+class Solution {
+    //OX퀴즈
+    public ArrayList<String> solution(String[] quiz) {
+        ArrayList<String> OXList = new ArrayList<>();
+
+        for(int i=0;i<quiz.length;i++ ){
+            String[] new_quiz=quiz[i].split(" ");
+            int fistValue = Integer.parseInt(new_quiz[0]);
+            int secondValue = Integer.parseInt(new_quiz[2]);
+            int resultValue = Integer.parseInt(new_quiz[4]);
+
+            int result;
+            if(new_quiz[1].equals("+")){
+                result = fistValue + secondValue;
+            }else {
+                result = fistValue - secondValue;
+            }
+
+            if(result == resultValue){
+                OXList.add("O");
+            }else {
+                OXList.add("X");
+            }
+        }
+        return OXList;
+    }
+}
