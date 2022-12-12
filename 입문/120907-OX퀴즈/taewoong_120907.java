4691afdb1ab992578c8edc0bc1d9358cf4d8b214
taewoong Kim <ktw2172@gmail.com>
2022-12-12 17:11:16 +0900

    code: Solve programmers 120887 K의개수 (taewoong)

--- 입문/120907-OX퀴즈/taewoong_120907.java
+++ 입문/120907-OX퀴즈/taewoong_120907.java
@@ -2,23 +2,20 @@
 class Solution {
     public String[] solution(String[] quiz) {
         String[] answerArr = new String[quiz.length];
-        // 정답 배열 만들기
         for(int i =0; i<quiz.length; i++){
             String[] splitArr = quiz[i].split(" ");
             //공백으로 나누기
             if (splitArr[1].equals("+")){
                 //+연산자일 경우
-                int compare = Integer.parseInt(splitArr[0]) + Integer.parseInt(splitArr[2]);
-                if (compare == Integer.parseInt(splitArr[4]))
-                    answerArr[i] = "O";
-                else answerArr[i] ="X";
+                int compareNum = Integer.parseInt(splitArr[0]) + Integer.parseInt(splitArr[2]);
+                if (compareNum == Integer.parseInt(splitArr[4]))
+                    answerArr[i] = "O"; else answerArr[i] ="X";
             }
             else if(splitArr[1].equals("-")){
                 //-연산자일 경우
-                int compare = Integer.parseInt(splitArr[0]) - Integer.parseInt(splitArr[2]);
-                if (compare == Integer.parseInt(splitArr[4]))
-                    answerArr[i] = "O";
-                else answerArr[i] ="X";
+                int compareNum = Integer.parseInt(splitArr[0]) - Integer.parseInt(splitArr[2]);
+                if (compareNum == Integer.parseInt(splitArr[4]))
+                    answerArr[i] = "O"; else answerArr[i] ="X";
             }
         }
         return answerArr;
