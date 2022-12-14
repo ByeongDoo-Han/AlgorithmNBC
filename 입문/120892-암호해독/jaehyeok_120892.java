5c6de52db10d5b64d7c14066e243a315b79c6259
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-14 12:21:44 +0900

    code : Solve programmers 암호해독(jaehyeok)

--- 입문/120892-암호해독/jaehyeok_120892.java
+++ 입문/120892-암호해독/jaehyeok_120892.java
@@ -1,1 +1,17 @@
 // git commit -m "code: Solve programmers 120892 암호해독 (jaehyeok)"
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder answer = new StringBuilder();
+        int count =0;
+        char[] cipherToChar = cipher.toCharArray();
+        for (char c :cipherToChar ) {
+            ++count;
+            if (count%code==0){
+                answer.append(Character.toString(c));
+            }
+        }
+
+
+        return answer.toString();
+    }
+}
