8f1ae45d00acb5904e5b4d9a91a6f67d32fe42e6
J-Jaeh <1wo2gur@naver.com>
2022-12-15 10:00:52 +0900

    code : Solve programers 120884 치킨쿠폰 (jaehyeok)

--- 입문/120884-치킨쿠폰/jaehyeok_120884.java
+++ 입문/120884-치킨쿠폰/jaehyeok_120884.java
@@ -1,1 +1,8 @@
 // git commit -m "code: Solve programmers 120884 치킨쿠폰 (jaehyeok)"
+
+class Solution_치킨쿠폰 {
+    public int solution(int chicken) {
+        double serviceChicken = chicken*0.1111111;
+        return (int)serviceChicken;
+    }
+}
