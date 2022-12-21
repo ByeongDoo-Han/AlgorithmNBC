3677058026c3472cefb0ce90901f7e676d77de6a
J-Jaeh <1wo2gur@naver.com>
2022-12-21 23:37:28 +0900

    code : Solve programmers 120929 각도기 (jaehyeok)

--- 입문/120929-각도기/jaehyeok_120929.java
+++ 입문/120929-각도기/jaehyeok_120929.java
@@ -1,1 +1,15 @@
-// git commit -m "code: Solve programmers 120929 각도기 (jaehyeok)"
+// git commit -m "code:class Solution {
+//    publ Solve programmers 120929 각도기 (jaehyeok)"
+
+class Solution {
+    public int solution(int angle) {
+        if(angle<90){
+            return 1;
+        }else if (angle==90){
+            return 2;
+        }else if (angle<180){
+            return 3;
+        }else
+            return 4;
+    }
+}
