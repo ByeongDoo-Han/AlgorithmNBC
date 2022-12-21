90c9644d6df7a942d07a4c7148ff96dca9fa10a5
J-Jaeh <1wo2gur@naver.com>
2022-12-21 23:44:26 +0900

    code: Solve programmers 120583 중복된숫자개수 (jaehyeok)

--- 입문/120583-중복된숫자개수/jaehyeok_120583.java
+++ 입문/120583-중복된숫자개수/jaehyeok_120583.java
@@ -1,1 +1,13 @@
 // git commit -m "code: Solve programmers 120583 중복된숫자개수 (jaehyeok)"
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int count = 0;
+        for(int i : array){
+            if (i ==n){
+                count +=1;
+            }
+        }
+        return count;
+    }
+}
