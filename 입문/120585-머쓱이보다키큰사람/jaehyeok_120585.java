71b135ad276b78cb14369f33b3a338828efd61f1
J-Jaeh <1wo2gur@naver.com>
2022-12-21 23:45:48 +0900

    code: Solve programmers 120585 머쓱이보다키큰사람 (jaehyeok)

--- 입문/120585-머쓱이보다키큰사람/jaehyeok_120585.java
+++ 입문/120585-머쓱이보다키큰사람/jaehyeok_120585.java
@@ -1,1 +1,13 @@
 // git commit -m "code: Solve programmers 120585 머쓱이보다키큰사람 (jaehyeok)"
+
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int i : array){
+            if(i > height)
+                answer ++;
+        }
+
+        return answer;
+    }
+}
