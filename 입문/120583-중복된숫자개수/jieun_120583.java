dc242beb1bc35053c62dbf3fc17ae10226c8438b
Song Jieun <jieuns2max@gmail.com>
2022-12-21 15:54:32 +0900

    jieun update

--- 입문/120583-중복된숫자개수/jieun_120583.java
+++ 입문/120583-중복된숫자개수/jieun_120583.java
@@ -1,1 +1,11 @@
-// git commit -m "code: Solve programmers 120583 중복된숫자개수 (jieun)"
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == n){
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
