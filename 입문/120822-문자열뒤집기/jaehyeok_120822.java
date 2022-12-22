e9d6daead7e18cef9ca225d1a8bb5c2ebe972d3d
J-Jaeh <1wo2gur@naver.com>
2022-12-23 00:57:18 +0900

    git commit -m "code: Solve programmers 120822 문자열뒤집기 (jaehyeok)

--- 입문/120822-문자열뒤집기/jaehyeok_120822.java
+++ 입문/120822-문자열뒤집기/jaehyeok_120822.java
@@ -1,1 +1,18 @@
 // git commit -m "code: Solve programmers 120822 문자열뒤집기 (jaehyeok)"
+
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] num_list) {
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for(int i = num_list.length-1 ; i>=0;i--){
+
+            answer.add(num_list[i]);
+
+        }
+
+        return answer;
+    }
+}
