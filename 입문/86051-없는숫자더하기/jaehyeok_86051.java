452b2621c3fd01ace585712b77f6091a36453f99
J-Jaeh <1wo2gur@naver.com>
2022-12-23 01:03:14 +0900

    code: Solve programmers 86051 없는숫자더하기 (jaehyeok)

--- 입문/86051-없는숫자더하기/jaehyeok_86051.java
+++ 입문/86051-없는숫자더하기/jaehyeok_86051.java
@@ -1,1 +1,29 @@
 // git commit -m "code: Solve programmers 86051 없는숫자더하기 (jaehyeok)"
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer =0;
+        ArrayList<Integer> number = new ArrayList<>();
+        for (int i : numbers) {
+            number.add(i);
+        }
+
+        HashSet<Integer> allNumber = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+        HashSet<Integer> comparNumber = new HashSet<>(number);
+
+        allNumber.removeAll(comparNumber);
+
+        ArrayList<Integer> answers =new ArrayList<>(allNumber);
+
+        for (int i = 0; i < answers.size(); i++) {
+            answer += answers.get(i);
+        }
+
+        return answer;
+    }
+}
