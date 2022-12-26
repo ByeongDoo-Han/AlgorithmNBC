79d9c6e85cef4a37d9772327d06c100bb2cdeafb
J-Jaeh <1wo2gur@naver.com>
2022-12-27 00:22:59 +0900

    code: Solve programmers 120847 최댓값만들기1 (jaehyeok)

--- 입문/120847-최댓값만들기1/jaehyeok_120847.java
+++ 입문/120847-최댓값만들기1/jaehyeok_120847.java
@@ -1,1 +1,17 @@
 // git commit -m "code: Solve programmers 120847 최댓값만들기1 (jaehyeok)"
+import java.util.ArrayList;
+import java.util.Comparator;
+
+class Solution {
+    public int solution(int[] numbers) {
+        ArrayList<Integer> newNumbers = new ArrayList<>();
+
+        for (int number : numbers) {
+            newNumbers.add(number);
+        }
+
+        newNumbers.sort(Comparator.naturalOrder());
+
+        return newNumbers.get(newNumbers.size()-1)*newNumbers.get(newNumbers.size()-2);
+    }
+}
