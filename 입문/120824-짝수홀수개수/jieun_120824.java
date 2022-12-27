c0857973f151f223366c24963e174a9a01c1a763
dorong0226 <113877929+dorong0226@users.noreply.github.com>
2022-12-27 20:32:19 +0900

    Update jieun_120824.java

--- 입문/120824-짝수홀수개수/jieun_120824.java
+++ 입문/120824-짝수홀수개수/jieun_120824.java
@@ -1,1 +1,13 @@
-// git commit -m "code: Solve programmers 120824 짝수홀수개수 (jieun)"
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        for(int i = 0; i < num_list.length; i++){
+            if (num_list[i] % 2 == 0){
+                answer[0] += 1;
+            }else{
+                answer[1] += 1;
+            }
+        }
+         return answer;
+    }
+}
