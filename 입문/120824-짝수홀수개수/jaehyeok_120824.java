cef20fe79f37b1df96b34f14e0a9991665be175e
J-Jaeh <1wo2gur@naver.com>
2022-12-28 00:44:09 +0900

    "code: Solve programmers 120824 짝수홀수개수 (jaehyeok)

--- 입문/120824-짝수홀수개수/jaehyeok_120824.java
+++ 입문/120824-짝수홀수개수/jaehyeok_120824.java
@@ -1,1 +1,14 @@
 // git commit -m "code: Solve programmers 120824 짝수홀수개수 (jaehyeok)"
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int countEven = 0;
+        for (int i : num_list) {
+            if(i%2==0){
+                countEven++;
+            }
+        }
+        int[] answer = {countEven,num_list.length-countEven};
+        return  answer;
+    }
+}
