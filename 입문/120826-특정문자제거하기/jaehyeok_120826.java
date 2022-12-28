cca3f11fad7e34756ad450b3e0e02eb86c1f01a0
J-Jaeh <1wo2gur@naver.com>
2022-12-29 01:01:16 +0900

    code: Solve programmers 120826 특정문자제거하기 (jaehyeok)

--- 입문/120826-특정문자제거하기/jaehyeok_120826.java
+++ 입문/120826-특정문자제거하기/jaehyeok_120826.java
@@ -1,1 +1,7 @@
 // git commit -m "code: Solve programmers 120826 특정문자제거하기 (jaehyeok)"
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = my_string.replace(letter,"");
+        return answer;
+    }
+}
