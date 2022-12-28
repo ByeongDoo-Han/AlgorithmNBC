6fcdd946623178357c3e7971589bd74c71ad4c23
J-Jaeh <1wo2gur@naver.com>
2022-12-29 01:02:14 +0900

    code: Solve programmers 120825 문자반복출력하기 (jaehyeok)

--- 입문/120825-문자반복출력하기/jaehyeok_120825.java
+++ 입문/120825-문자반복출력하기/jaehyeok_120825.java
@@ -1,1 +1,23 @@
 // git commit -m "code: Solve programmers 120825 문자반복출력하기 (jaehyeok)"
+class Solution {
+    public StringBuffer solution(String my_string, int n) {
+
+
+        StringBuffer answer = new StringBuffer();
+
+
+        char[] newStirng =  my_string.toCharArray();
+
+        int count = 0;
+
+        for (char c : newStirng) {
+            while (count<n){
+                answer.append(c);
+                count++;
+            }
+            count=0;
+        }
+
+        return answer;
+    }
+}
