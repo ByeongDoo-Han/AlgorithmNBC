0e04a7f51c34d0b3d328688936845aa1ea74691f
Lee Sol <leeso4567@naver.com>
2023-01-10 15:50:36 +0900

    code: Solve programmers 120899 가장큰수찾기 (sol)

--- 입문/120899-가장큰수찾기/sol_120899.java
+++ 입문/120899-가장큰수찾기/sol_120899.java
@@ -1,1 +1,24 @@
 // git commit -m "code: Solve programmers 120899 가장큰수찾기 (sol)"
+
+package programmers;
+
+public class FindMaxNum { //프로그래머스 입문 <가장 큰 수 찾기>
+    //https://school.programmers.co.kr/learn/courses/30/lessons/120899
+    public static int[] solution(int[] array) {
+        int[] answer = new int[2];
+
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > answer[0]) {
+                answer[0] = array[i];
+                answer[1] = i;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
