e5b7f9cac6944adc3897472464f43bf9eec75fd8
Lee Sol <leeso4567@naver.com>
2023-01-10 15:43:44 +0900

    code: Solve programmers 120897 약수구하기 (sol)

--- 입문/120897-약수구하기/sol_120897.java
+++ 입문/120897-약수구하기/sol_120897.java
@@ -1,1 +1,22 @@
 // git commit -m "code: Solve programmers 120897 약수구하기 (sol)"
+
+import java.util.ArrayList;
+
+public class Divisor { //프로그래머스 입문 <약수 구하기>
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120897
+
+    public static ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0)
+                answer.add(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
