51b4e0dcfeef40e9d9560a5795515fcb09d883c8
J-Jaeh <1wo2gur@naver.com>
2023-01-11 02:25:21 +0900

    code: Solve programmers 120897 약수구하기 (jaehyeok)

--- 입문/120897-약수구하기/jaehyeok_120897.java
+++ 입문/120897-약수구하기/jaehyeok_120897.java
@@ -1,1 +1,15 @@
 // git commit -m "code: Solve programmers 120897 약수구하기 (jaehyeok)"
+
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> answer =new ArrayList<>();
+        for(int i =1;i<n+1;i++){
+            if(n%i==0){
+                answer.add(i);
+            }
+        }
+        return answer;
+    }
+}
