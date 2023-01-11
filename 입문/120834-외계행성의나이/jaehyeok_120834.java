e8e9e6546abac8988c3947b69c4f155415de65dc
J-Jaeh <1wo2gur@naver.com>
2023-01-12 02:31:31 +0900

    code: Solve programmers 120834 외계행성의나이 (jaehyeok)

--- 입문/120834-외계행성의나이/jaehyeok_120834.java
+++ 입문/120834-외계행성의나이/jaehyeok_120834.java
@@ -1,1 +1,16 @@
 // git commit -m "code: Solve programmers 120834 외계행성의나이 (jaehyeok)"
+class Solution {
+    public StringBuffer  solution(int age) {
+        StringBuffer answer = new StringBuffer();
+
+        String stringAge= age+"";
+
+        char[] chars = stringAge.toCharArray();
+
+        for (char c : chars) {
+            answer.append((char)(c+49));
+        }
+
+        return answer;
+    }
+}
