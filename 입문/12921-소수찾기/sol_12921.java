393cb3b3ccb12783a630f30d5dae99486e6ca6ba
Lee Sol <leeso4567@naver.com>
2023-01-13 16:52:03 +0900

    code: Solve programmers 12921 소수찾기 (sol)

--- 입문/12921-소수찾기/sol_12921.java
+++ 입문/12921-소수찾기/sol_12921.java
@@ -1,1 +1,18 @@
 // git commit -m "code: Solve programmers 12921 소수찾기 (sol)"
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n+1];
+        for(int i = 2; i < n + 1; i++) {
+            for(int j = i; j < n + 1; j = j + i) {
+                arr[j]++;
+            }
+        }
+        for(int i = 2; i < arr.length; i++) {
+            if (arr[i] == 1){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
