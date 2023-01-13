43aa5c0c5ea86457f749fea3543bad3ea45ffbfc
J-Jaeh <1wo2gur@naver.com>
2023-01-14 03:22:53 +0900

    code: Solve programmers 120891 369게임 (jaehyeok)

--- 입문/120891-369게임/jaehyeok_120891.java
+++ 입문/120891-369게임/jaehyeok_120891.java
@@ -1,1 +1,17 @@
 // git commit -m "code: Solve programmers 120891 369게임 (jaehyeok)"
+
+class Solution {
+    public int solution(int order) {
+        int count = 0 ;
+
+        while (order>0){
+
+            if((order%10)%3==0 && order%10!=0){
+                ++count;
+            }
+            order/=10;
+        }
+
+        return count;
+    }
+}
