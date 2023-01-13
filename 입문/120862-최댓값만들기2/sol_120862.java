c1f9007a27e8eaf060b0f1cabf6b79d09db0e764
Lee Sol <leeso4567@naver.com>
2023-01-13 17:06:56 +0900

    code: Solve programmers 120862 최댓값만들기2 (sol)

--- 입문/120862-최댓값만들기2/sol_120862.java
+++ 입문/120862-최댓값만들기2/sol_120862.java
@@ -1,1 +1,15 @@
 // git commit -m "code: Solve programmers 120862 최댓값만들기2 (sol)"
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = Integer.MIN_VALUE;
+        for(int i = 0; i < numbers.length-1; i++) {
+            for(int j = i+1; j < numbers.length; j++) {
+                if(numbers[i] * numbers[j] > answer) {
+                    answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return answer;
+    }
+}
