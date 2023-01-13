10c91ad3f69fecd238d3d549b5606bd2327db2a8
Lee Sol <leeso4567@naver.com>
2023-01-13 17:07:42 +0900

    code: Solve programmers 68935 3진법뒤집기 (sol)

--- 입문/68935-3진법뒤집기/sol_68935.java
+++ 입문/68935-3진법뒤집기/sol_68935.java
@@ -1,1 +1,18 @@
 // git commit -m "code: Solve programmers 68935 3진법뒤집기 (sol)"
+class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+        StringBuilder tmp = new StringBuilder();
+
+        while(n > 0) {
+            tmp.append((char)(n%3 + 48));
+            n = n / 3;
+        } //뒤집어진 3진법 수 구함
+
+        for(int i = 0; i < tmp.length(); i++) {
+            answer += Math.pow(3, tmp.length() - i - 1) * Character.getNumericValue(tmp.charAt(i));
+        }
+
+        return answer;
+    }
+}
