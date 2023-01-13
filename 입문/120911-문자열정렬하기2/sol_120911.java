e33d740c69375eb473b0f4571e92b54646e425a9
Lee Sol <leeso4567@naver.com>
2023-01-13 16:53:06 +0900

    code: Solve programmers 120911 문자열정렬하기2 (sol)

--- 입문/120911-문자열정렬하기2/sol_120911.java
+++ 입문/120911-문자열정렬하기2/sol_120911.java
@@ -1,1 +1,21 @@
 // git commit -m "code: Solve programmers 120911 문자열정렬하기2 (sol)"
+class Solution {
+    public static String solution(String my_string) {
+        //우선 소문자로 바꾸고, sort 사용 ?
+        String answer = "";
+        char[] ch = my_string.toLowerCase().toCharArray();
+
+        for(int i = 0; i < ch.length - 1; i++) {
+            for(int j = i + 1; j < ch.length; j++) {
+                if (ch[j] < ch[i]) {
+                    char tmp = ch[i];
+                    ch[i] = ch[j];
+                    ch[j] = tmp;
+                }
+            }
+        }
+        answer = String.valueOf(ch);
+
+        return answer;
+    }
+}
