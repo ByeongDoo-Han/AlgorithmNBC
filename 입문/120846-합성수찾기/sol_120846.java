30c7fa4df14648f79ff6cf4ea72e540789a3716a
Lee Sol <leeso4567@naver.com>
2023-01-13 16:49:49 +0900

    code: Solve programmers 120846 합성수찾기 (sol)

--- 입문/120846-합성수찾기/sol_120846.java
+++ 입문/120846-합성수찾기/sol_120846.java
@@ -1,1 +1,20 @@
 // git commit -m "code: Solve programmers 120846 합성수찾기 (sol)"
+class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+        int count;
+        for(int i = 4; i <= n; i++) {
+            count = 0;
+            for(int j = 1; j <= i; j++) {
+                if(i % j == 0) {
+                    count++;
+                    if(count > 2) {
+                        answer++;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
